eccbbc82a0b08fc57b0a05ebcab265ba0d8071f4
Scott Watermasysk <scottwater@gmail.com>
2020-03-11 22:37:50 -0400

    mac layout for bottom row mods

--- keyboards/lazydesigners/dimple/keymaps/scottwater/keymap.c
+++ keyboards/lazydesigners/dimple/keymaps/scottwater/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------------------------------------------------------------|
  * | Shift  | Z  | X  | C  | V  | B  | N  | M  | ,  | Up |    .    |
  * |---------------------------------------------------------------|
- *      |Ctrl|Gui |Alt | Spc/Lwr |   Spc/Rse   |Left|Down|Rght|
+ *      |Ctrl|Alt |Gui | Spc/Lwr |   Spc/Rse   |Left|Down|Rght|
  *      `-----------------------------------------------------'
  */
 
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
 		KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,
 		SFT_CPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
-		KC_LCTL, KC_LGUI, KC_LALT, SPC_LOW,  SPC_UPR,  KC_LEFT, KC_DOWN, KC_RGHT
+		KC_LCTL, KC_LALT, KC_LGUI, SPC_LOW,  SPC_UPR,  KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
 /*
